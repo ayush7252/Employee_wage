7d616bc1d0b0b174474833185e60a1ca20fb1864
Ayush <ayush0340.be21@chitkara.edu.in>
2024-12-04 15:40:48 +0530

    [Ayush] Mod : Condition for wages

--- Employee_Wages.js
+++ Employee_Wages.js
@@ -4,6 +4,7 @@ const PerHrWage = 20;
 let TotalWage = 0;
 let TtotalHr = 0;
 let ans = generate();
+let days = 1;
 
 function generate(){
     const num =  Math.floor(Math.random() * 2);
@@ -35,11 +36,20 @@ function CalMonthWage(){
     }
 }
 
+function CalConditionWage(){
+    while(TtotalHr <= 160 && days <20){
+        days++;
+        CalculateWages();
+    }
+}
+
 if(ans === 1){
    console.log("present"); 
-   CalMonthWage();
-   console.log(`Total Wage for a Month ${TotalWage}`);
+   CalConditionWage()
+   console.log("Limit Reached! Either No. of Days Exceed or No. of working Hours Exceeded ");
+   console.log(`Total Wage for a Month : ${TotalWage}`);
    console.log(`Total Hours for a Month : ${TtotalHr}`);
+   console.log(`Total Days for a Month : ${days}`); 
 }else if(ans === 0){
    console.log("absent");
 }
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ UC2 : Calculating Daily Wages Using Switch Cases
 UC3 : Refactor the CalculateWages for calculating the working Hours of employee also 
 ----------------------------------------------------------------
 UC4 : Create a New function for calculating monthly Wage.
+----------------------------------------------------------------
+UC5 : Create a new function for checking the condition wheather the given conditions are true or not.
+----------------------------------------------------------------
