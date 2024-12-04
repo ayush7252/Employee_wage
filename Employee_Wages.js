ae5144b6bbb7a4d678500544d6e261cf39ba674a
Ayush <ayush0340.be21@chitkara.edu.in>
2024-12-04 11:52:02 +0530

    [Ayush] Mod : Calculating Daily Wages Of Employee

--- Employee_Wages.js
+++ Employee_Wages.js
@@ -1,3 +1,7 @@
+const partHr = 4;
+const fullHr = 8;
+const PerHrWage = 20;
+let TotalWage = 0;
 let ans = generate();
 
 function generate(){
@@ -5,8 +9,27 @@ function generate(){
     return num;
 }
 
+function generate2(){
+    const num =  Math.floor(Math.random() * 3);
+    return num;
+}
+
+function CalculateWages( n = generate2()){
+    switch(n) {
+        case 0: return
+        case 1:
+            TotalWage += partHr * PerHrWage;
+            break;
+        case 2:
+            TotalWage += fullHr * PerHrWage;
+            break;
+    }
+}
+
 if(ans === 1){
    console.log("present"); 
+   CalculateWages();
+   console.log(`Total Wage for a Day : ${TotalWage}`);
 }else if(ans === 0){
    console.log("absent");
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Employee_wage
 
 UC1 : implementing a attendance feature by using Math.random Method
+----------------------------------------------------------------
+UC2 : Calculating Daily Wages Using Switch Cases 
